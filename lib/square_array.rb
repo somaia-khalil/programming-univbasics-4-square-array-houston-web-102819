3c3d101293ffa2a00a007074ddc76ea8470f7785
somaia khalil <sumaya_khair@live.com>
2019-10-15 14:44:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,9 @@ def square_array(array)
   counter = 0 
   while array[counter] < array.length do
   newarray =  array[counter] ** 2 
-  puts newarray
+  
     counter+=1 
   end
+  return newarray
 end
 square_array(number)
